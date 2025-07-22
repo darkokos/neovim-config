9a2f484d7e39029d2594d08bc101c94928818852
darkokos <darkosvilar02@gmail.com>
2025-07-22 02:30:51 +0200

    [add] lazydev, tokyonight theme and tokyonight lualine

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,15 @@
+return {
+	'folke/tokyonight.nvim',
+	priority = 1000,
+	config = function()
+		---@diagnostic disable-next-line: missing-fields
+		require('tokyonight').setup({
+			styles = {
+				comments = { italic = false },
+			},
+			transparent = true,
+		})
+
+		vim.cmd.colorscheme('tokyonight-moon')
+	end,
+}
