005eeb5adf10416d95f0dc906b1f04b6073044eb
darkokos <darkosvilar02@gmail.com>
2025-09-11 18:41:00 +0200

    [add] qol options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,25 @@ vim.o.number = true
 vim.o.relativenumber = true
 
 vim.o.tabstop = 4
+
+vim.o.showmode = false
+
+vim.schedule(function ()
+	vim.o.clipboard = 'unnamedplus'
+end)
+
+vim.o.breakindent = true
+
+vim.o.undofile = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.signcolumn = 'yes'
+
+vim.o.updatetime = 250
+
+vim.o.timeoutlen = 300
+
+vim.o.splitright = true
+vim.o.splitbelow = true
