3776c5038f1636f8e1fb039947299b86a2d557ae
darkokos <darkosvilar02@gmail.com>
2025-09-12 02:19:30 +0200

    [add] basic keymaps

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,16 @@
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!"<CR>')
+
+vim.keymap.set('n', '<leader>h', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<leader>l', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<leader>j', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<leader>k', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.keymap.set('n', '<leader>H', '<C-w>H', { desc = 'Move window to the left' })
+vim.keymap.set('n', '<leader>L', '<C-w>L', { desc = 'Move window to the right' })
+vim.keymap.set('n', '<leader>J', '<C-w>J', { desc = 'Move window down' })
+vim.keymap.set('n', '<leader>K', '<C-w>K', { desc = 'Move window up' })
