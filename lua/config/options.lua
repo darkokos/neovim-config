31ea74d27c89437d2aecbe2191f60fbdaf3ced04
darkokos <darkosvilar02@gmail.com>
2025-09-15 01:12:30 +0200

    [add] tabstop and shiftwidth options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,12 +1,15 @@
 vim.o.number = true
 vim.o.relativenumber = true
 
-vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.tabstop = 8
+vim.o.expandtab = false
 
 vim.o.showmode = false
 
-vim.schedule(function ()
-	vim.o.clipboard = 'unnamedplus'
+vim.schedule(function()
+	vim.o.clipboard = "unnamedplus"
 end)
 
 vim.o.breakindent = true
@@ -16,14 +19,14 @@ vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
-vim.o.signcolumn = 'yes'
+vim.o.signcolumn = "yes"
 
 vim.o.updatetime = 250
 
 vim.o.splitright = true
 vim.o.splitbelow = true
 
-vim.o.inccommand = 'split'
+vim.o.inccommand = "split"
 
 vim.o.cursorline = true
 
