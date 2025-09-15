00adf4e3695e88f5286515c485bd5a5e3cb7bed8
darkokos <darkosvilar02@gmail.com>
2025-09-15 13:39:54 +0200

    [fix] more intuitive telescope keybinds

--- lua/plugins/telescope.nvim.lua
+++ lua/plugins/telescope.nvim.lua
@@ -29,9 +29,9 @@ return {
 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
 		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+		vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[S]earch existing [B]uffers" })
 		vim.keymap.set("n", "<leader>sn", function()
 			builtin.find_files({ cwd = vim.fn.stdpath("config") })
-		end, { desc = "[ ] Find existing buffers" })
+		end, { desc = "[S]earch [N]eovim files" })
 	end,
 }
