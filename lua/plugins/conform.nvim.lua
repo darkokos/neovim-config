a5fcddad5d4f13e6d5f07d72a5908a56ca1f8f5f
darkokos <darkosvilar02@gmail.com>
2026-03-11 18:18:34 +0100

    [add] more conform formatters

--- lua/plugins/conform.nvim.lua
+++ lua/plugins/conform.nvim.lua
@@ -5,6 +5,18 @@ return {
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
+			css = { "prettierd", "prettier", stop_after_first = true },
+			graphql = { "prettierd", "prettier", stop_after_first = true },
+			html = { "prettierd", "prettier", stop_after_first = true },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+			json = { "prettierd", "prettier", stop_after_first = true },
+			less = { "prettierd", "prettier", stop_after_first = true },
+			markdown = { "prettierd", "prettier", stop_after_first = true },
+			scss = { "prettierd", "prettier", stop_after_first = true },
+			typescript = { "prettierd", "prettier", stop_after_first = true },
+			typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+			yaml = { "prettierd", "prettier", stop_after_first = true },
 		},
 		format_on_save = {
 			timeout_ms = 500,
