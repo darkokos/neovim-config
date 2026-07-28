c87231c642ff88d4f4af9b88e37496db1c1c612c
darkokos <darkosvilar02@gmail.com>
2026-07-29 01:44:49 +0200

    [fix] switched to new treesitter api

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,9 +1,10 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
 	build = ":TSUpdate",
-	main = "nvim-treesitter.configs",
-	opts = {
-		ensure_installed = {
+	config = function()
+		local treesitter = require("nvim-treesitter")
+		treesitter.install({
 			"bash",
 			"c",
 			"diff",
@@ -15,12 +16,43 @@ return {
 			"query",
 			"vim",
 			"vimdoc",
-		},
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = { "ruby" },
-		},
-		indent = { enable = true, disable = { "ruby" } },
-	},
+		})
+
+		local function treesitter_try_attach(buf, language)
+			if not vim.treesitter.language.add(language) then
+				return
+			end
+
+			vim.treesitter.start(buf, language)
+
+			local has_indent_query = vim.treesitter.query.get(language, "indents") ~= nil
+			if has_indent_query then
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end
+		end
+
+		local available_parsers = treesitter.get_available()
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(args)
+				local buf, filetype = args.buf, args.match
+
+				local language = vim.treesitter.language.get_lang(filetype)
+				if not language then
+					return
+				end
+
+				local installed_parsers = treesitter.get_installed("parsers")
+
+				if vim.tbl_contains(installed_parsers, language) then
+					treesitter_try_attach(buf, language)
+				elseif vim.tbl_contains(available_parsers, language) then
+					treesitter.install(language):await(function()
+						treesitter_try_attach(buf, language)
+					end)
+				else
+					treesitter_try_attach(buf, language)
+				end
+			end,
+		})
+	end,
 }
